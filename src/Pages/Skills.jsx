e9bb9e79bb96cb0da046c3b2a4b2e214ab337f5b
R-U-Bharti <kumar.bharti@gtropy.com>
2024-06-05 12:03:48 +0530

    skills

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -16,29 +16,73 @@ import socket from '../assets/logo/Socket IO.png'
 import tailwind from '../assets/logo/Taiwind CSS.png'
 import skillBackground from '../assets/skillBackground.webp'
 import './Skills.css'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const Skills = () => {
 
   const [toggle, setToggle] = useState(true)
+  const [item, setItem] = useState([])
+  const [item2, setItem2] = useState([])
+
+  const circleRef1 = useRef(null)
+  const circleRef2 = useRef(null)
+
+  useEffect(() => {
+    if (circleRef1.current) {
+      const circle = circleRef1.current;
+      const radius = circle.offsetWidth / 2;
+      const itemRadius = 50; // items width (100px/2) = 50
+      const itemsCount = 16;
+      const newItems = [];
+
+      for (let i = 0; i < itemsCount; i++) {
+        const angle = (i / itemsCount) * (2 * Math.PI);
+        // const x = radius + (radius - itemRadius) * Math.cos(angle) - itemRadius;
+        const x = radius + radius * Math.cos(angle);
+        // const y = radius + (radius - itemRadius) * Math.sin(angle) - itemRadius;
+        const y = radius + radius * Math.sin(angle);
+
+        newItems.push({ x, y });
+      }
+      setItem(newItems);
+    }
+
+    if (circleRef2.current) {
+      const circle = circleRef2.current;
+      const radius = circle.offsetWidth / 2;
+      const itemRadius = 50; // items width (100px/2) = 50
+      const itemsCount = 16;
+      const newItems = [];
+
+      for (let i = 0; i < itemsCount; i++) {
+        const angle = (i / itemsCount) * (2 * Math.PI);
+        const x = radius + radius * Math.cos(angle);
+        const y = radius + radius * Math.sin(angle);
+
+        newItems.push({ x, y });
+      }
+      setItem2(newItems);
+    }
+
+  }, [circleRef1.current, circleRef2.current])
 
   let logoList = [
-    { id: 1, name: 'React.js', image: reactJs, position: 'top-[0%] left-[40%]' },
-    { id: 2, name: 'React Native', image: reactNative, position: 'top-[0%] left-[40%]' },
-    { id: 3, name: 'Next.js', image: next, position: 'top-[0%] left-[40%]' },
-    { id: 4, name: 'Tailwind CSS', image: tailwind, position: 'top-[0%] left-[40%]' },
+    { id: 1, name: 'Javascript', image: js, position: 'top-[0%] left-[40%]' },
+    { id: 2, name: 'MongoDB', image: mongo, position: 'top-[0%] left-[40%]' },
+    { id: 3, name: 'Socket.io', image: socket, position: 'top-[0%] left-[40%]' },
+    { id: 4, name: 'Next.js', image: next, position: 'top-[0%] left-[40%]' },
     { id: 5, name: 'Bootstrap CSS', image: bootstrap, position: 'top-[0%] left-[40%]' },
-    { id: 6, name: 'Docker', image: docker, position: 'top-[0%] left-[40%]' },
-    { id: 7, name: 'Git', image: git, position: 'top-[0%] left-[40%]' },
-    { id: 8, name: 'Node.js', image: node, position: 'top-[0%] left-[40%]' },
-    { id: 9, name: 'Express.js', image: express, position: 'top-[0%] left-[40%]' },
-    { id: 10, name: 'HTML 5', image: html, position: 'top-[0%] left-[40%]' },
-    { id: 11, name: 'CSS 3', image: css, position: 'top-[0%] left-[40%]' },
-    { id: 12, name: 'Javascript', image: js, position: 'top-[0%] left-[40%]' },
-    { id: 13, name: 'PostgreSQL', image: pg, position: 'top-[0%] left-[40%]' },
-    { id: 14, name: 'MongoDB', image: mongo, position: 'top-[0%] left-[40%]' },
-    { id: 15, name: 'Socket.io', image: socket, position: 'top-[0%] left-[40%]' },
-    { id: 16, name: 'Redux', image: redux, position: 'top-[0%] left-[40%]' },
+    { id: 6, name: 'Git', image: git, position: 'top-[0%] left-[40%]' },
+    { id: 7, name: 'Node.js', image: node, position: 'top-[0%] left-[40%]' },
+    { id: 8, name: 'HTML 5', image: html, position: 'top-[0%] left-[40%]' },
+    { id: 9, name: 'CSS 3', image: css, position: 'top-[0%] left-[40%]' },
+    { id: 10, name: 'Express.js', image: express, position: 'top-[0%] left-[40%]' },
+    { id: 11, name: 'React.js', image: reactJs, position: 'top-[0%] left-[40%]' },
+    { id: 12, name: 'React Native', image: reactNative, position: 'top-[0%] left-[40%]' },
+    { id: 13, name: 'Tailwind CSS', image: tailwind, position: 'top-[0%] left-[40%]' },
+    { id: 14, name: 'Redux', image: redux, position: 'top-[0%] left-[40%]' },
+    { id: 15, name: 'PostgreSQL', image: pg, position: 'top-[0%] left-[40%]' },
+    { id: 16, name: 'Docker', image: docker, position: 'top-[0%] left-[40%]' },
   ]
 
   logoList?.sort((a, b) => a.id - b.id);
@@ -48,7 +92,7 @@ const Skills = () => {
   return (
     <div className='md:h-[70vh] w-screen md:pt-[19.5rem] md:pb-0 pb-10'>
       <div className='md:h-[70vh] relative w-screen flex md:items-end justify-center'>
-      
+
         <div className="md:hidden flex flex-wrap justify-center gap-10 -mt-16 ">
           {logoList?.map((elem, index) => (
             <div className="flex flex-col pl-16 md:pl-0 md:items-center md:justify-end w-full md:w-[calc(100%/8)] gap-2" key={index}>
@@ -63,25 +107,26 @@ const Skills = () => {
             </div>
           ))}
         </div>
-      
-        <div onMouseEnter={() => setToggle(false)} onMouseLeave={() => setToggle(true)} className={`${toggle ? 'skillList1' : 'stopSkillList1'} md:block hidden rotation1 absolute w-[70vw] h-[70vw] bottom-[-14vw] transition-all duration-300 ease-in-out rounded-full border border-gray-50 dark:border-gray-100`}>
-          {logoList?.map((elem, index) => (
-            <div className={`${skillCard} hover:scale-150 group transition-all duration-200 skill_card ${index > 7 ? `skill_card${elem?.id}bottom` : `skill_card${elem?.id}top`} flex flex-col pl-10 md:pl-0 md:items-center md:justify-end w-full md:w-[calc(100%/10)] gap-2`} key={index}>
-              <img
-                className='md:w-[4vw] w-[80%] dark:saturate-200 dark:hover:saturate-150 border border-gray-300 group-hover:bg-white dark:border-gray-500 h-[7rem] rounded-full md:h-[4vw] p-4 object-contain bg-[#fffffff0] dark:bg-[#fff]/40  shadow-[0px_0px_10px_rgba(255,255,255,0.5)] dark:shadow-[0px_0px_10px_rgba(0,0,0,0.5)]'
-                src={elem.image}
-                alt="Logo"
-                width={100}
-                height={100}
-              />
-              <div className='md:w-[10vw] w-[80%] text-center font-semibold text-md dark:text-gray-50 text-gray-800'>{elem?.name}</div>
-            </div>
+
+        <div ref={circleRef1} onMouseEnter={() => setToggle(false)} onMouseLeave={() => setToggle(true)} className={`${toggle ? 'skillList1' : ''} md:block hidden absolute w-[70vw] h-[70vw] bottom-[-14vw] transition-all duration-300 ease-in-out rounded-full border border-gray-50 dark:border-gray-100`}>
+          {item?.length > 0 && logoList?.map((elem, index) => (
+            <>
+              <div style={{ position: 'absolute', translate: '-50% -50%', left: `${item[index]?.x}px`, top: `${item[index]?.y}px` }} className={`${skillCard} skill_card border border-gray-300 dark:border-gray-500 rounded-full w-[70px] h-[70px] hover:bg-white hover:scale-150 group transition-all duration-200 flex flex-col md:items-center md:justify-end gap-2  bg-[#fffffff0] dark:bg-[#fff]/40  shadow-[0px_0px_10px_rgba(255,255,255,0.5)] dark:shadow-[0px_0px_10px_rgba(0,0,0,0.5)]`} key={index}>
+                <img
+                  className='w-[100px] h-[100px] p-0.5 object-contain'
+                  src={elem.image}
+                  alt="Logo"
+                />
+                <div style={{ transform: 'translate(0%, 150%)' }} className='absolute text-center font-semibold text-md dark:text-gray-50 text-gray-800'>{elem?.name}</div>
+              </div>
+            </>
           ))}
         </div>
 
-        <div onMouseEnter={() => setToggle(false)} onMouseLeave={() => setToggle(true)} className={`${toggle ? 'skillList2' : 'stopSkillList2'} md:block hidden absolute  w-[55vw] h-[55vw] bottom-[-7vw] transition-all duration-300 ease-in-out rounded-full border border-gray-50 dark:border-gray-100`}>
-          {logoList?.map((elem, index) => (
-            <div className={`${skillCard} hover:scale-150 group transition-all duration-200 skill_card ${index < 8 ? `skill_card${elem?.id}bottom` : `skill_card${elem?.id}top`} flex flex-col pl-10 md:pl-0 md:items-center md:justify-end w-full md:w-[calc(100%/10)] gap-2`} key={index}>
+        <div ref={circleRef2} onMouseEnter={() => setToggle(false)} onMouseLeave={() => setToggle(true)} className={`${toggle ? 'skillList2' : ''} md:block hidden absolute w-[55vw] h-[55vw] bottom-[-7vw] transition-all duration-300 ease-in-out rounded-full border border-gray-50 dark:border-gray-100`}>
+          {item2?.length > 0 && logoList?.reverse()?.map((elem, index) => (
+            <>
+              {/* <div className={`${skillCard} hover:scale-150 group transition-all duration-200 skill_card ${index < 8 ? `skill_card${elem?.id}bottom` : `skill_card${elem?.id}top`} flex flex-col pl-10 md:pl-0 md:items-center md:justify-end w-full md:w-[calc(100%/10)] gap-2`} key={index}>
               <img
                 className='md:w-[4vw] w-[80%] dark:saturate-200 dark:hover:saturate-150 border border-gray-300 group-hover:bg-white dark:border-gray-500 h-[7rem] rounded-full md:h-[4vw] p-4 object-contain bg-[#fffffff0] dark:bg-[#fff]/40  shadow-[0px_0px_10px_rgba(255,255,255,0.5)] dark:shadow-[0px_0px_10px_rgba(0,0,0,0.5)]'
                 src={elem.image}
@@ -90,12 +135,22 @@ const Skills = () => {
                 height={100}
               />
               <div className='md:w-[10vw] w-[80%] text-center font-semibold text-md dark:text-gray-50 text-gray-800'>{elem?.name}</div>
-            </div>
+            </div> */}
+
+              <div style={{ position: 'absolute', translate: '-50% -50%', left: `${item2[index]?.x}px`, top: `${item2[index]?.y}px` }} className={`${skillCard} skill_card border border-gray-300 dark:border-gray-500 rounded-full w-[70px] h-[70px] hover:bg-white hover:scale-150 group transition-all duration-200 flex flex-col md:items-center md:justify-end gap-2  bg-[#fffffff0] dark:bg-[#fff]/40  shadow-[0px_0px_10px_rgba(255,255,255,0.5)] dark:shadow-[0px_0px_10px_rgba(0,0,0,0.5)]`} key={index}>
+                <img
+                  className='w-[100px] h-[100px] p-0.5 object-contain'
+                  src={elem.image}
+                  alt="Logo"
+                />
+                <div style={{ transform: 'translate(0%, 150%)' }} className='absolute text-center font-semibold text-md dark:text-gray-50 text-gray-800'>{elem?.name}</div>
+              </div>
+            </>
           ))}
         </div>
 
         <div className="w-[40vw] h-[40vw] md:block hidden absolute bottom-0 border rounded-full">
-          <img  draggable="false" onMouseEnter={() => setToggle(false)} onMouseLeave={() => setToggle(true)} src={skillBackground} className='w-[40vw] h-[20vw] overflow-clip transition-all duration-300 ease-in-out transform z-10 border-t-2 border-l-2 border-r-2 border-[#ffffff4d] rounded-t-full' alt="" srcset="" />
+          <img draggable="false" onMouseEnter={() => setToggle(false)} onMouseLeave={() => setToggle(true)} src={skillBackground} className='w-[40vw] h-[20vw] overflow-clip transition-all duration-300 ease-in-out transform z-10 border-t-2 border-l-2 border-r-2 border-[#ffffff4d] rounded-t-full' alt="" srcset="" />
         </div>
 
       </div>
