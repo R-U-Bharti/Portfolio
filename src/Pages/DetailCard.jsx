0179df6672a7823a2f65f2ad00c81027071b9a87
R-U-Bharti <kumar.bharti@gtropy.com>
2024-06-05 15:06:59 +0530

    home

--- src/Pages/DetailCard.jsx
+++ src/Pages/DetailCard.jsx
@@ -4,11 +4,11 @@ import { RxCross2 } from 'react-icons/rx'
 const DetailCard = ({ image, heading, content, color, close }) => {
     return (
         <div className='w-full h-full flex justify-center items-center select-none'>
-            <div style={{ borderColor: color }} className={` relative md:m-0 m-2 w-full bg-[#fff]/80 dark:bg-[#000]/80 border-2 p-4 md:max-w-[60%] grid grid-cols-12 gap-4`}>
+            <div style={{ borderColor: color }} className={` relative md:m-0 m-2 w-full max-h-[95vh] overflow-auto bg-[#fff]/80 dark:bg-[#000]/80 border-2 p-4 md:max-w-[60%] grid grid-cols-12 gap-4`}>
 
-                <div onClick={() => close()} className='absolute top-0 right-0 bg-red-400 hover:bg-red-500 p-1'><RxCross2 /></div>
+                <div onClick={() => close()} className='absolute cursor-pointer top-0 right-0 bg-red-400 hover:bg-red-500 p-1'><RxCross2 /></div>
 
-                <img className='col-span-12 md:col-span-5 bg-white/40 rounded-md h-full object-contain' src={image} alt="image" srcset="" />
+                <img style={{backgroundColor: `${color}33`}} className='col-span-12 md:col-span-5 p-2 rounded-md h-full object-contain' src={image} alt="image" srcset="" />
 
                 <div className="col-span-12 md:col-span-7">
                     <h1 style={{ color: color }} className='font-bold text-2xl'>{heading}</h1>
